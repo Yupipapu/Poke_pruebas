3a70271e2792dfc6a718f2f6d23237e8b25fa749
Yupipapu <72059639+Yupipapu@users.noreply.github.com>
2026-09-06 18:21:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const path = require('path');
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const { db, runAsync, getAsync, allAsync, initDatabase } = require('./database');
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 const app = express();
 app.use(cors());
 app.use(express.json());
