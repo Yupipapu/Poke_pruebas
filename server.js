0eaa83cd9691f297d6b796be3e0b7a98923b383d
Yupipapu <72059639+Yupipapu@users.noreply.github.com>
2026-09-06 18:27:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const { db, runAsync, getAsync, allAsync, initDatabase } = require('./database');
 
-const PORT = process.env.PORT || 3000;
+PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
