2618d13508bc8f7febaebdf45ffdbbdc65626ba3
Yupipapu <72059639+Yupipapu@users.noreply.github.com>
2026-09-07 22:52:42 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'database.db');
+const dbDir = process.env.RAILWAY_VOLUME_MOUNT_PATH || __dirname;
+const dbPath = path.join(dbDir, 'database.db');
+
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error al abrir la base de datos:", err.message);
